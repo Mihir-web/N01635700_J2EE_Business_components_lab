ca9fcd3477f71fda186c7ae183c9923dd8e748aa
Mihir-web <hiteshhirpara3862@gmail.com>
2025-02-25 10:09:12 -0500

    Test 3 Code

---
+++ N01635700_Hirpara-Test3-IMS/src/main/java/com/n01635700/n01635700_hirparatest3ims/N01635700HirparaTest3ImsApplication.java
@@ -0,0 +1,13 @@
+package com.n01635700.n01635700_hirparatest3ims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N01635700HirparaTest3ImsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(N01635700HirparaTest3ImsApplication.class, args);
+    }
+
+}
---
+++ N01635700_Hirpara-Test3-IMS/src/test/java/com/n01635700/n01635700_hirparatest3ims/N01635700HirparaTest3ImsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n01635700.n01635700_hirparatest3ims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N01635700HirparaTest3ImsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
