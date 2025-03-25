b259cfebda9a4af0620168fe6dc3a03e0980a367
Mihir-web <hiteshhirpara3862@gmail.com>
2025-03-25 10:28:24 -0400

    Lab 10 Added

---
+++ Lab10/src/test/java/com/n01635700/lab10/Lab10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n01635700.lab10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
